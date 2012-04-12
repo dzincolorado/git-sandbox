d33aaa2f509d6c4190fbbfc00501dd1c060922aa
Denis Ziegler <dzincolorado@yahoo.com>
2012-04-11 23:05:37 -0600

    console message

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var http = require('http')
 http.createServer(onRequest).listen(8888);
 
 function onRequest(request, response){
+	writeToConsoleWrapper(writeToConsole, "Request received");
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World");
-	writeToConsoleWrapper(writeToConsole, "this is now in a named method!");
 	response.end();
 }
 
@@ -18,3 +18,5 @@ function writeToConsoleWrapper(aFunction, word)
 {
 	aFunction(word);
 }
+
+writeToConsoleWrapper(writeToConsole, "Server started.");
