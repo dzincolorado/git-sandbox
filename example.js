9fad13b2cd64f2d73ece92d9933510789ce47c1c
Denis Ziegler <dzincolorado@yahoo.com>
2012-04-11 20:49:29 -0600

    update spacing

--- example.js
+++ example.js
@@ -6,4 +6,5 @@ http.createServer(function(request, response)
 	response.end('This is my first simple nodejs server and I like it!\n');
 }).listen(8124);
 
+
 console.log('server running at 127.0.0.1:8124');
