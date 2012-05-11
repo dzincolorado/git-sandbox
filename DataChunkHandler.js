1db94a5c28f5b642c817c91a66df2e0c2d8e0850
Denis Ziegler <dzincolorado@yahoo.com>
2012-05-10 23:12:56 -0600

    adding data chunk handling, session with redis store

---
+++ DataChunkHandler.js
@@ -0,0 +1,25 @@
+/**
+ * @author dziegler
+ */
+//var exec = require('child_process').exec;
+var querystring = require('querystring');
+
+function HandleDataChunks(request, response)
+{
+	request.setEncoding('utf8');
+	
+	var postData = "";
+	request.addListener("data", function(postDataChunk) {
+		postData +=	postDataChunk;
+		console.log("Received POST data chunk" + postDataChunk + "'.");
+		
+	});
+	
+	request.addListener("end", function(postDataChunk){
+		//route the request
+		//route(handle, pathname, response, postData)
+		return querystring.parse(postData).text;
+	});
+}
+
+exports.HandleDataChunks = HandleDataChunks;
